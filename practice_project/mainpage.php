3e82aecaf67e3d6fd5adcf585a6120638cc641b9
baechanghyeon12 <qo99033@naver.com>
2023-04-23 22:08:39 +0900

    0423

---
+++ practice_project/mainpage.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>헬스에 관하여</p>
+    <table>
+        <thead>
+            <tr>
+                <th>
+                    팔
+                </th>
+                <th>
+                    복근
+                </th>
+                <th>
+                    허벅지
+                </th>
+                <th>
+                    종아리
+                </th>
+                <th>
+                    등
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>
+
+                </td>
+                <td>
+
+                </td>
+                <td>
+
+                </td>
+                <td>
+
+                </td>
+                <td>
+
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>
